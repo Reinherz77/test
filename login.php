930859673288e5b0b5c1f8aef03112695314b9c2
Reinherz77 <hafiz08214@gmail.com>
2022-07-02 01:57:29 +0700

    changing html to php

---
+++ login.php
@@ -0,0 +1,34 @@
+<div class="container-field">
+    <div class="row">
+        <div class="col-6 bg-light">
+
+        </div>
+        <div class="col-6" style="padding:260px 65px 260px">
+        <div class="container">
+            <h1 class="my-5 text-center">Selamat Datang</h1>
+            <p class="my-3">Silahkan masukkan username dan password</p>
+            <from method='post'>
+                <div class="row mb-3">
+                    <label class="col-2 col-from-label align-self-center">Username</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="username" placeholder="username">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label class="col-2 col-from-label align-self-center">Password</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" name="password" placeholder="password">
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-2">
+                    </div>
+                    <div class="col-10">
+                        <input type="submit" class="col-5 btn btn-prmary mr-5" name="submit" value="login">
+                        <button type="button" class="col-5 btn btn-prmary btn-light" onclick="window.history.back()">Batal</button>        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
